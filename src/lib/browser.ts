26e55d3713fec11bea8f0a1ebd308fa0a29864a7
Marais Rossouw <me@marais.co>
2020-11-06 11:00:21 +1000

    chore: getting a start on browser

--- src/lib/browser.ts
+++ src/lib/browser.ts
@@ -2,6 +2,25 @@ export async function* generate<T>(
 	stream: ReadableStream<Uint8Array>,
 	boundary: string,
 ): AsyncGenerator<T> {
-	debugger;
+	const isPreamble = true;
+
+	let buffer: Uint8Array;
 	const reader = stream.getReader();
+
+	try {
+		let done;
+
+		do {
+			const result = await reader.read();
+			done = result.done;
+
+			if (!buffer) {
+				buffer = result.value;
+			}
+
+			debugger;
+		} while (!done);
+	} finally {
+		reader.releaseLock();
+	}
 }
--- tests/browser.spec.ts
+++ tests/browser.spec.ts
@@ -1,3 +1,8 @@
+import { ServerResponse } from 'http';
+import { join } from 'path';
+import { chromium, Page } from 'playwright';
+import { rollup } from 'rollup';
+import typescript from 'rollup-plugin-typescript2';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
@@ -6,10 +11,64 @@ import { configureServer } from './server';
 
 configureServer(test);
 
+test.before(async (context) => {
+	context.browser = await chromium.launch({
+		headless: !process.env.DEBUG,
+		slowMo: 10,
+		devtools: !!process.env.DEBUG,
+		args: ['--disable-web-security'],
+	});
+	const browsingContext = await context.browser.newContext();
+	context.page = await browsingContext.newPage();
+});
+test.after((context) => context.browser.close());
+
+test.before(async (context) => {
+	const build = await rollup({
+		input: join(__dirname, '../src/browser.ts'),
+		plugins: [
+			typescript({
+				check: false,
+				tsconfigOverride: {
+					declaration: false,
+				},
+			}),
+		],
+	});
+
+	const mod = await build.generate({ format: 'module' });
+
+	context.server.get('meros.mjs', (_req: any, res: ServerResponse) => {
+		res.writeHead(200, {
+			'Content-Type': `text/javascript`,
+		});
+		res.end(mod.output.find((i) => i.type === 'chunk').code);
+	});
+});
+
 test('exports', () => {
 	assert.type(meros.meros, 'function');
 });
 
-test('can make a call', async (context) => {});
+test('can make a call', async (context) => {
+	await (context.page as Page).setContent(`
+	<html>
+	<body></body>
+<script type="module">
+	import { meros } from "http://localhost:${context.port}/meros.mjs";
+
+	(async function() {
+		const parts = await fetch("http://localhost:${context.port}/mock-ep").then(meros);
+
+		for await (let part of parts) {
+			debugger;
+		}
+	})();
+
+</script>
+	</html>
+	`);
+	debugger;
+});
 
 test.run();
