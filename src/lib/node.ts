592d8e184a2501fbc88b56227d2494f317da050b
Luke Edwards <luke.edwards05@gmail.com>
2020-11-06 10:32:58 -0800

    chore: minor tweaks to node

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -13,54 +13,50 @@ export async function* generate<T>(
 	for await (const chunk of stream) {
 		buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length);
 		const boundaryIndex = buffer.indexOf(boundary);
+		if (!~boundaryIndex) continue;
 
-		if (isPreamble && !!~boundaryIndex) {
-			isPreamble = false;
+		if (isPreamble) {
 			buffer = buffer.slice(boundaryIndex + boundary.length);
+			isPreamble = false;
 			continue;
 		}
 
-		if (!!~boundaryIndex) {
-			const payload = buffer.slice(0, boundaryIndex);
-			const headerSeparated = payload.indexOf(separator);
-			let data = payload.slice(headerSeparated);
+		const payload = buffer.slice(0, boundaryIndex);
+		const headerSeparated = payload.indexOf(separator);
+		let data = payload.slice(headerSeparated);
 
-			const headers: Map<string, string> = new Map();
-			for (let item of payload
-				.slice(0, headerSeparated)
-				.toString('utf8')
-				.trim()
-				.split(/\r\n/)) {
-				const idx = item.indexOf(':');
-				headers.set(
-					item.slice(0, idx).toLowerCase(),
-					item.slice(idx + 1).trim(),
-				);
-			}
-
-			data = data.slice(data.indexOf(separator) + separator.length);
-
-			if (headers.has('content-length')) {
-				data = data.slice(
-					0,
-					parseInt(headers.get('content-length'), 10),
-				);
-			}
-
-			if (!isJson)
-				isJson =
-					headers.has('content-type') &&
-					!!~headers.get('content-type').indexOf('application/json');
-
-			yield isJson
-				? JSON.parse(data.toString('utf8'))
-				: data.toString('utf8');
-
-			if (!!~buffer.indexOf(boundary + '--')) {
-				break;
-			}
-
-			buffer = buffer.slice(boundaryIndex + boundary.length);
+		const headers: Map<string, string> = new Map();
+		for (let item of payload
+			.slice(0, headerSeparated)
+			.toString('utf8')
+			.trim()
+			.split(/\r\n/)) {
+			const idx = item.indexOf(':');
+			headers.set(
+				item.slice(0, idx).toLowerCase(),
+				item.slice(idx + 1).trim(),
+			);
 		}
+
+		data = data.slice(data.indexOf(separator) + separator.length);
+
+		const clength = headers.get('content-length');
+		if (clength != null) {
+			data = data.slice(0, parseInt(clength, 10));
+		}
+
+		if (!isJson) {
+			isJson = !!~(headers.get('content-type') || '').indexOf(
+				'application/json',
+			);
+		}
+
+		yield isJson
+			? JSON.parse(data.toString('utf8'))
+			: data.toString('utf8');
+
+		if (!!~buffer.indexOf(boundary + '--')) break;
+
+		buffer = buffer.slice(boundaryIndex + boundary.length);
 	}
 }
