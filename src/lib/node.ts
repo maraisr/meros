d534da4e6654bc7a932afb034f1232ff47f69953
Luke Edwards <luke.edwards05@gmail.com>
2020-11-10 08:55:32 -0800

    fix(node): exhaust `next` buffer parts

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -12,7 +12,7 @@ export async function* generate<T>(
 	let is_preamble = true;
 	let is_json = false;
 
-	for await (const chunk of stream) {
+	outer: for await (const chunk of stream) {
 		let idx_boundary = buffer.length;
 		buffer = Buffer.concat([buffer, chunk]);
 		const idx_chunk = (chunk as Buffer).indexOf(boundary);
@@ -31,38 +31,40 @@ export async function* generate<T>(
 			}
 		}
 
-		const next = buffer.slice(idx_boundary + boundary_length);
-		const current = buffer.slice(0, idx_boundary);
+		while (!!~idx_boundary) {
+			const current = buffer.slice(0, idx_boundary);
+			const next = buffer.slice(idx_boundary + boundary_length);
 
-		if (is_preamble) {
-			buffer = next;
-			is_preamble = false;
-			continue;
+			if (is_preamble) {
+				is_preamble = false;
+			} else {
+				let ctype = '', clength = '';
+				const idx_headers = current.indexOf(separator);
+
+				// parse headers, only keeping relevant headers
+				buffer.slice(0, idx_headers).toString().trim().split(/\r\n/).forEach((str, idx) => {
+					idx = str.indexOf(':');
+					let key = str.substring(0, idx).toLowerCase();
+					if (key === 'content-type') ctype = str.substring(idx + 1).trim();
+					else if (key === 'content-length') clength = str.substring(idx + 1).trim();
+				});
+
+				let payload = current.slice(idx_headers + separator.length);
+				if (clength) payload = payload.slice(0, parseInt(clength, 10));
+
+				is_json = ctype === '' ? is_json : !!~ctype.indexOf('application/json');
+
+				yield is_json
+					? JSON.parse(payload.toString())
+					: payload.toString();
+
+				if (next.slice(0, 2).toString() === '--') {
+					break outer;
+				}
+			}
+
+			buffer=next; last_index=0;
+			idx_boundary = buffer.indexOf(boundary);
 		}
-
-		let ctype = '', clength = '';
-		const idx_headers = current.indexOf(separator);
-
-		// parse headers, only keeping relevant headers
-		buffer.slice(0, idx_headers).toString().trim().split(/\r\n/).forEach((str, idx) => {
-			idx = str.indexOf(':');
-			let key = str.substring(0, idx).toLowerCase();
-			if (key === 'content-type') ctype = str.substring(idx + 1).trim();
-			else if (key === 'content-length') clength = str.substring(idx + 1).trim();
-		});
-
-		let payload = current.slice(idx_headers + separator.length);
-		if (clength) payload = payload.slice(0, parseInt(clength, 10));
-
-		is_json = ctype === '' ? is_json : !!~ctype.indexOf('application/json');
-
-		yield is_json
-			? JSON.parse(payload.toString())
-			: payload.toString();
-
-		if (next.slice(0, 2).toString() === '--') break;
-
-		buffer = next;
-		last_index = 0;
 	}
 }
