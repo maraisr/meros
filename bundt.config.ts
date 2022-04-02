d5da30da9044afdf954d21b8f236f79ccb84482e
Luke Edwards <luke.edwards05@gmail.com>
2022-04-02 10:41:46 -0700

    fix: build es2019 target

--- bundt.config.ts
+++ bundt.config.ts
@@ -1,5 +1,7 @@
 import { define } from 'bundt/config';
 
 export default define((input, options) => {
+	options.target = 'es2019';
+	// ignore "index.ts" build attempt
 	if (input.export === '.') return false;
 });
