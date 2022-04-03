c9f84fdc9c77c00b95cf4234e94c06a5c1266d7b
Marais Rossouw <me@marais.co>
2022-04-04 09:39:25 +1000

    chore: es2019 not really a target

--- bundt.config.ts
+++ bundt.config.ts
@@ -1,7 +1,6 @@
 import { define } from 'bundt/config';
 
-export default define((input, options) => {
-	options.target = 'es2019';
+export default define((input) => {
 	// ignore "index.ts" build attempt
 	if (input.export === '.') return false;
 });
